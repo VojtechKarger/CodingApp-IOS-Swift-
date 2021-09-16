826a8409c2a0fda8190aafbe4c84034cc213ab10
vojta <vojta@ne.cz>
2021-09-16 18:22:18 +0200

    Initial Commit

---
+++ CodingGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CodingGame
+//
+//  Created by vojta on 16.09.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
